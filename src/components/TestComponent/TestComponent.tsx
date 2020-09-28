d91508746c8d6e19b707e5833ce28df03100678c
Mike Theodorou <c-michael.theodorou@charter.com>
2020-09-27 20:46:12 -0600

    adds react navigation base library

---
+++ src/components/TestComponent/TestComponent.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function TestComponent() {
+  return (
+    <View>
+      <Text>Bang Bang</Text>
+    </View>
+  );
+}
+
+export default TestComponent;
